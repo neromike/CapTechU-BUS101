3cf5bab9647b6b3f18acd5ea1e6473ff46d3782a
Wayne Pereanu <github@pereanu.com>
2022-11-19 11:33:02 -0500

    Create introduction_exercise.py

---
+++ Week1/introduction_exercise.py
@@ -0,0 +1,28 @@
+# For this exercise, you will need to change the value in this section of
+# program.
+# 
+# This assignment will require that you enter your name in the string below.
+# So if your name is "Isabel", then the line of code should look like:
+#     your_name = "Isabel"
+# 
+# After you've made this change, save the file.
+# Once saved, you must then run the program using the Python interepreter.
+# 
+# For this program and the rest of this class, you can safely ignore
+# everything after the commented line with ten dashes.
+
+your_name = "Isabel"
+
+# ----------
+
+import string
+import sys
+try:
+    from sklearn import preprocessing
+except ModuleNotFoundError:
+    print("You do not have sklearn installed with this Python interepreter.")
+    sys.exit()
+
+le = preprocessing.LabelEncoder()
+le.fit(list(''.join([i for i in your_name.lower() if i.isalpha()])))
+print( sum([ord(c) for c in le.classes_]) )
